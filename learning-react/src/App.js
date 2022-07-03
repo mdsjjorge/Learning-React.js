b3891eb779e3fc978a1044355583ae38194c6d21
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-03 16:27:05 -0300

    useEffect, useReducer, checker

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState, useReducer} from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  // emotion é nossa State variável
-  // setEmotion é a função que usaremos para atualizar o State
+
+  const [checked, setChecked] = useReducer(checked => !checked, false);
+
+// ===============================
+// useEffect
+// ===============================
+  // const [emotion, setEmotion] = useState("happy");
+  // const [secondary, setSecondary] = useState("tired");
+
+  // useEffect(() => {
+  //   console.log(`It's ${emotion} right now`);
+  // }, [emotion, secondary]);
+
   return (
     <div className='App'>
-      <h1>Current emotion is {emotion}</h1>
+      <input type="checkbox" value={checked} onChange={setChecked} />
+      <label> {checked? "checked" : "not-checked"}</label> 
+
+      {/* <h1>Current emotion is {emotion}</h1>
       <button onClick={() => {setEmotion("Sad")}}>Sad</button>
       <button onClick={() => {setEmotion("Excited")}}>Excited</button>
       <button onClick={() => {setEmotion("happy")}}>back</button>
+      <h2> current secondary emotion is {secondary}.</h2>
+      <button onClick={() => {setSecondary("Grateful")}}>Grateful</button>
+      <button onClick={() => {setSecondary("tired")}}>back</button> */}
+
     </div>
   );
 }

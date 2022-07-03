e3481d9ae73b03341037245114b275afbdc6a02a
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-03 11:36:18 -0300

    react destructuring01

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// =========== destructuring =============
+    // const [a,b,c] = [
+    //   "tokio",
+    //   "Fortal",
+    //   "Stm"
+    // ];
+    // console.log(b);
+// =======================================
+
+function App({Library}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello world! with {Library}</h1>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 

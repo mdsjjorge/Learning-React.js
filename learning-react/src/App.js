6b3261a02d7e4973de0bcaad9b75bfd053094450
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-04 15:40:21 -0300

    working with render props]

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,42 +1,39 @@
 import './App.css';
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
 
-function GithubUser({name, location, avatar}) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar} height={250}/>
-    </div>
-  )
+const tahoe_peaks = [
+  { name: "Freel", elevation: 1087}, 
+  { name: "Monumental", elevation: 3227}, 
+  { name: "Pyramid", elevation: 9987}, 
+  { name: "Tallac", elevation: 3227} 
+];
+
+function List ({data, renderItem, renderEmpty}) {
+    return !data.length ? (
+      renderEmpty
+    ) : (
+      <ul>
+          {data.map((item) => (
+              <li key= {item.name}>
+                  {renderItem(item)}
+              </li>
+          ))}
+      </ul>
+    );
 }
 
 function App() {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(null);
-    useEffect(() => {
-      setLoading(true);
-      fetch(
-        'https://api.github.com/users/mdsjjorge'
-      )
-        .then((response) => response.json())
-        .then(setData)
-        .then(() => setLoading(false))
-        .catch(setError);
-    }, []);
 
-    if (loading) return <h1> Loading... </h1>;
-    if (error)
-      return <pre> {JSON.stringify(error)} </pre>
-    if(!data) return null;
-    
       return (
-          <GithubUser 
-              name= {data.name} 
-              location = {data.location}
-              avatar = {data.avatar_url}
-          />
+        <List 
+          data= {tahoe_peaks}
+          renderEmpty={<p> This list is empty</p>}
+          renderItem={(item) => (
+            <>
+              {item.name} - {item.elevation} ft.
+            </>
+        )}
+        />
       );
 }
 

d16ae78751d47eb8c521101e01a89fccf19b3d92
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-04 18:51:18 -0300

    installing and configuring Router

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,40 +1,30 @@
 import './App.css';
-// import {useState, useEffect} from "react";
 
-const tahoe_peaks = [
-  { name: "Freel", elevation: 1087}, 
-  { name: "Monumental", elevation: 3227}, 
-  { name: "Pyramid", elevation: 9987}, 
-  { name: "Tallac", elevation: 3227} 
-];
-
-function List ({data, renderItem, renderEmpty}) {
-    return !data.length ? (
-      renderEmpty
-    ) : (
-      <ul>
-          {data.map((item) => (
-              <li key= {item.name}>
-                  {renderItem(item)}
-              </li>
-          ))}
-      </ul>
-    );
+function Home() {
+  return (
+    <div>
+      <h1> My website</h1>
+    </div>
+  )
 }
 
-function App() {
-
-      return (
-        <List 
-          data= {tahoe_peaks}
-          renderEmpty={<p> This list is empty</p>}
-          renderItem={(item) => (
-            <>
-              {item.name} - {item.elevation} ft.
-            </>
-        )}
-        />
-      );
+export function About() {
+  return (
+    <div>
+      <h1> About us</h1>
+    </div>
+  )
 }
 
-export default App;
+export function Contact() {
+  return (
+    <div>
+      <h1> Contact us</h1>
+    </div>
+  )
+}
+
+export function App() {
+      return <Home/>
+}
+

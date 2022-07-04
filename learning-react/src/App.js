a9a4d8b19e2e65d912f8d224a23c6914554657ce
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-04 11:19:57 -0300

    useRef + forms with uncontrolled component

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,35 +1,36 @@
 import './App.css';
-import {useEffect, useState, useReducer} from "react";
+import {useRef} from "react";
+// useRef is a hook that is going to reach out to some sort of UI
+// element and get its value
+
 
 function App() {
+    const txtTitle = useRef();
+    const hexColor = useRef();
 
-  const [checked, setChecked] = useReducer(checked => !checked, false);
-
-// ===============================
-// useEffect
-// ===============================
-  // const [emotion, setEmotion] = useState("happy");
-  // const [secondary, setSecondary] = useState("tired");
-
-  // useEffect(() => {
-  //   console.log(`It's ${emotion} right now`);
-  // }, [emotion, secondary]);
-
-  return (
-    <div className='App'>
-      <input type="checkbox" value={checked} onChange={setChecked} />
-      <label> {checked? "checked" : "not-checked"}</label> 
-
-      {/* <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => {setEmotion("Sad")}}>Sad</button>
-      <button onClick={() => {setEmotion("Excited")}}>Excited</button>
-      <button onClick={() => {setEmotion("happy")}}>back</button>
-      <h2> current secondary emotion is {secondary}.</h2>
-      <button onClick={() => {setSecondary("Grateful")}}>Grateful</button>
-      <button onClick={() => {setSecondary("tired")}}>back</button> */}
-
-    </div>
-  );
+    const submit = (e) => {
+         e.preventDefault();
+         const title = txtTitle.current.value;
+         const color = hexColor.current.value;
+         alert(`${title}, ${color}`)
+         txtTitle.current.value = ""
+         hexColor.current.value = ""
+    };
+    return (
+        <form onSubmit={submit} >
+          <input 
+            ref={txtTitle}
+            type = "text"
+            placeholder = "color"
+          />
+          <input 
+            ref={hexColor}
+            type = "color"
+          />
+          <button> Add </button>
+          {/* this forms is creating uncontrolled component */}
+        </form>
+    );
 }
 
 export default App;

19edc938655da916378e4bf6918d8caf8e43aea3
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-04 14:43:15 -0300

    fetching data with hooks

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,41 +1,23 @@
 import './App.css';
-import {useState} from "react";
-// it is important to think about repeatable behavior to create a custom hook
-// custom hook is a function
+import {useState, useEffect} from "react";
 
-function useInput (initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-    {value, onChange: e => setValue(e.target.value)},
-    () => setValue(initialValue)
-  ]
-}
 
 function App() {
-    const [titleProps, resetTitle] = useInput("");
-    const [colorProps, resetColor] = useInput("#000000");
+    const [data, setData] = useState(null);
+    useEffect(() => {
+      fetch(
+        'https://api.github.com/users/mdsjjorge'
+      ).then((response) => response.json()).then(setData);
+    }, []);
+    if (data) 
+      return (
+         <pre>{JSON.stringify(data, null, 2)}</pre>
+      );
 
-    const submit = (e) => {
-         e.preventDefault();
-         alert(`${titleProps.value}, ${colorProps.value}`);
-         resetTitle();
-         resetColor();
-    };
     return (
-        <form onSubmit={submit} >
-          <input 
-            {...titleProps}
-            // ... sintax to spread all of the properties
-            type = "text"
-            placeholder = "color"
-          />
-          <input 
-            {...colorProps}
-            type = "color"
-          />
-          <button> Add </button>
-          {/* this forms is creating uncontrolled component */}
-        </form>
+        <h1>
+
+        </h1>
     );
 }
 
